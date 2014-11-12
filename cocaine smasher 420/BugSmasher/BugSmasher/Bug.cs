e0aa2c753688d88e8b4e27b4c4647932a819ece8
Miccioc <cmiccioc@gmail.com>
2014-11-12 08:05:49 -0500

    6

---
+++ cocaine smasher 420/BugSmasher/BugSmasher/Bug.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BugSmasher
+{
+    enum BugMoods
+    {
+        Timid,
+        Angry,
+        Normal
+    }
+
+
+    class Bug : Sprite
+    {
+        public BugMoods mood = BugMoods.Normal;
+        private Random rand = new Random((int)DateTime.UtcNow.Ticks);
+        float timeRemaining = 0.0f;
+        float TimePerNewTarget = 0.20f;
+        public Bug(
+           Vector2 location,
+           Texture2D texture,
+           Rectangle initialFrame,
+           Vector2 velocity)
+            : base(location, texture, initialFrame, velocity)
+        {
+            System.Threading.Thread.Sleep(2);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+            if (Location.Y < -50 && velocity.Y < 0) velocity *= new Vector2(1, -1);
+            if (Location.Y > 1700 && velocity.Y > 0) velocity *= new Vector2(-1, 1);
+
+
+            if (timeRemaining == 0.0f)
+            {
+                NewTarget();
+                timeRemaining = TimePerNewTarget;
+            }
+
+
+            timeRemaining = MathHelper.Max(0, timeRemaining - (float)gameTime.ElapsedGameTime.TotalSeconds);
+
+            base.Update(gameTime);
+        }
+
+        public void NewTarget()
+        {
+            Vector2 target = new Vector2(Location.X + 400, Location.Y + rand.Next(-100, 100));
+            Vector2 vel = target - Location;
+            vel.Normalize();
+            vel *= 90;
+            Velocity = vel;
+            Rotation = (float)Math.Atan2(vel.Y, vel.X);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+   ///if (mood == BugMoods.Angry)
+      ///      {
+           //     this.TintColor = Color.Red;
+            //    this.Velocity *= new Vector2(1.1f, 1f);
+
+             //   if (Velocity.Length() > 150)
+              //  {
+                //    this.velocity.Normalize();
+                //    this.velocity *= 150;
+               // }
+          //  }
+//else
+           // {
+            //    this.TintColor = Color.White;
+           // }
+          
+            base.Draw(spriteBatch);
+        }
+    }
+}
---
+++ cocaine smasher 420/BugSmasher/BugSmasher/Game1.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace BugSmasher
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D background, spritesheet;
+        Random rand = new Random(System.Environment.TickCount);
+        List<Bug> bugs = new List<Bug>();
+        int bugNum = 50;
+        Sprite hand;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferHeight = 988;
+            graphics.PreferredBackBufferWidth = 1680;
+            
+            graphics.ApplyChanges();
+            
+            Content.RootDirectory = "Content";
+
+            
+        }
+
+        
+        protected override void Initialize()
+        {
+            this.Window.Title=("BugSmasher");
+            this.Window.AllowUserResizing = true;
+
+            base.Initialize();
+        }
+
+
+        protected override void LoadContent()
+        {
+
+            background = Content.Load<Texture2D>("background");
+            spritesheet = Content.Load<Texture2D>("spritesheet");
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            hand = new Sprite(new Vector2 (100,100),
+                spritesheet,
+                new Rectangle(136 , 199 , 180 ,48),
+                new Vector2(100,100));
+            
+            for (int i = 0; i < bugNum; i++)
+            {
+                int bugX = rand.Next(0, 3);
+                int bugY = rand.Next(0, 2);
+                int Y = rand.Next(-40, 100);
+                if (Y == 10) Y=10;
+                Bug bug = new Bug(new Vector2 (rand.Next(-400,-50),rand.Next(50,400)), spritesheet, new Rectangle(64*bugX, 64*bugY, 64, 64), new Vector2(rand.Next(40, 150),Y));
+                bugs.Add(bug);
+            }
+            
+            
+        }
+
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+
+        protected override void Update(GameTime gameTime)
+        {
+            MouseState ms = Mouse.GetState();
+            hand.Location = new Vector2(ms.X, ms.Y);
+            
+         
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            
+            for (int i = 0; i < bugs.Count; i++)
+            {
+                bugs[i].Update(gameTime);
+                bugs[i].mood = BugMoods.Normal;
+                if (bugs[i].Location.X > this.Window.ClientBounds.Width) 
+                {
+                
+                }
+
+                for (int j = 0; j < bugNum; j++)
+                {
+                   
+                    if (bugs[i].IsBoxColliding(bugs[j].BoundingBoxRect))
+                    {
+                        bugs[i].mood = BugMoods.Angry;
+                    }
+                }
+            }
+
+            base.Update(gameTime);
+        }
+
+        public void Method(GameTime gameTime)
+        {
+            new Vector2(rand.Next(40, 150), rand.Next(-40, 40));
+            
+        
+        }
+        
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(background, new Rectangle(0, 0, this.Window.ClientBounds.Width, this.Window.ClientBounds.Height), Color.White); // Draw the background at (0,0) - no crazy tinting
+            hand.Draw(spriteBatch);
+            for (int i = 0; i < bugs.Count; i++)
+            {
+                bugs[i].Draw(spriteBatch);
+            }
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
